af9421eb2a48720ee458bfc0e09d5584e2664e5f
Jeremiah Dorvil <jeremiahdorvil@outlook.com>
2023-05-09 03:58:45 -0400

    finished 2 of 3 routes

--- models/index.js
+++ models/index.js
@@ -20,6 +20,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
+  as: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -27,6 +28,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
+  as: 'tag_id'
 });
 
 
